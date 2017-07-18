e9bf2ec771085bfe1a00b10ca4e727ea77a031c2
Cody Higgins <cody.higginsdev18@gmail.com>
2017-07-18 10:04:14 -0600

    added in user for sold customers, changed privacy page and reviews page

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,3 +52,65 @@ User.create!(
 )
 
  	puts "1 Estate user created"
+
+
+# Sold Customer logins##################################################################################
+
+
+
+User.create!(
+	email: "entity@pcs.com",
+	password: "Entity2017!",
+	password_confirmation: "Entity2017!",
+	roles: "entity",
+)
+
+ 	puts "1 Entity customer created"
+
+ User.create!(
+	email: "tax@pcs.com",
+	password: "Tax2017!",
+	password_confirmation: "Tax2017!",
+	roles: "tax",
+)
+
+ 	puts "1 Tax customer created"
+
+ User.create!(
+	email: "investors@pcs.com",
+	password: "Investor2017!",
+	password_confirmation: "Investor2017!",
+	roles: "investors",
+)
+
+ 	puts "1 Investors customer created"
+
+
+ User.create!(
+	email: "estate@pcs.com",
+	password: "Estate2017!",
+	password_confirmation: "Estate2017!",
+	roles: "estate",
+)
+
+ 	puts "1 Estate customer created"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
