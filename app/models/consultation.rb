304c9651e3fb86697fb6af3d8906d0b4ef3a86c4
Cody Higgins <cody.higginsdev18@gmail.com>
2017-07-25 11:11:39 -0600

    added in a piece of code that will strip all charaters from a phone number

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -2,4 +2,8 @@ class Consultation < ApplicationRecord
 
 validates_presence_of :first_name, :last_name, :number, :body
 
+  def number=(value)
+    super(value.blank? ? nil : value.gsub(/[^\w\s]/, '')) 
+  end
+
 end
