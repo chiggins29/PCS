5905b6ba12cc3bae2a373f7adbb5afc79a84d974
Cody Higgins <cody.higginsdev18@gmail.com>
2017-07-25 12:40:11 -0600

    added in code to the model to strip whitespaces and all special characters in phone number fields

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -3,7 +3,7 @@ class Consultation < ApplicationRecord
 validates_presence_of :first_name, :last_name, :number, :body
 
   def number=(value)
-    super(value.blank? ? nil : value.gsub(/[^\w\s]/, '')) 
+    super(value.blank? ? nil : value.strip.gsub(/[^0-9A-Za-z]/, '')) 
   end
 
 end
